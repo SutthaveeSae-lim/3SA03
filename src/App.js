4dfb6ad61733a9e54998dcb5931555c8314b1d49
SutthaveeSaelim <6210110379@psu.ac.th>
2022-08-13 21:02:18 +0700

    Part7

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
+import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 import React from 'react';
 import './App.css';
-import WordCard from './WordCard';
-
 const word = "Hello";
 function App() {
- return (
-  <div>
-    <WordCard value="hello"/>
-  </div>
- );
+  return (
+    <div className="App">
+    <WordCard value="Rarity"/>
+    </div>
+    );
 }
-
-export default App;
+export default App
