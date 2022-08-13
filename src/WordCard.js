665346bf8ccef4d0726798e6378158597a6d74a9
SutthaveeSaelim <6210110379@psu.ac.th>
2022-08-13 21:05:35 +0700

    Part7

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,44 +1,102 @@
 import React, { useState } from 'react';
-import CharacterCard from './CharacterCard'
 import _, { attempt } from 'lodash';
+import CharacterCard from './CharacterCard';
 
-const prepareStateFromWord = (given_word) => {
+const prepareStateFromWord = given_word => {
     let word = given_word.toUpperCase()
     let chars = _.shuffle(Array.from(word))
     return {
-    word,
-    chars,
-    attempt: 1,
-    guess: '',
-    completed: false
+        word,
+        chars,
+        attempt: 1,
+        guess: '',
+        completed: false
     }
 }
 
-export default function WordCard(props){
+export default function WordCard(props) {
+
     const [state, setState] = useState(prepareStateFromWord(props.value))
 
-    const activationHandler = (c) => {
+    const activationHandler = c => {
         console.log(`${c} has been activated.`)
 
         let guess = state.guess + c
-        setState({...state, guess})
+        setState({ ...state, guess })
 
-        if(guess.length == state.word.length){
-         if(guess == state.word){
-          console.log('yeah!')
-           setState({...state, guess: '', completed: true})
-        }else{
-          console.log('reset, next attempt')
-          setState({...state, guess: '', attempt: state.attempt + 1})
+        if (guess.length == state.word.length) {
+            if (guess == state.word) {
+                console.log('yeah!')
+                setState({ ...state, guess: guess, completed: true })
+            } else {
+                console.log('reset, next attempt')
+                setState({ ...state, guess: '', attempt: state.attempt + 1 })
+            }
         }
+        console.log(guess)
     }
-}
- return (
- <div>
- { 
-       state.chars.map((c, i) => 
-       <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}/>
-       )}
- </div>
- );
+
+    const resetWord = b => {
+        if (state.guess != '')
+            setState({ ...state, guess: '', attempt: state.attempt + 1, completed: false })
+    }
+
+    const congrade = state.completed ? 'CONGRATULATION' : ''
+
+
+
+    return (
+        <div >
+                <div style=
+                {{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}
+                className="congrade"
+                >
+
+                {'รอบที่ ' + state.attempt}
+            </div>
+            <br></br>
+            <center><img src="https://i.pinimg.com/originals/dd/a3/78/dda3788f383607bedec1eb840474a911.gif"/></center>
+
+            <center><div>
+                {
+                    state.chars.map((c, i) =>
+                        <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt} />)
+                }
+            </div></center>
+
+        
+            
+            <div className='font' style=
+                {{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                {state.guess}
+            </div>
+            <br></br>
+            
+            <div className='congrade' style=
+                {{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                {congrade}
+            </div>
+
+           <center> <div onClick={resetWord} className='card' style=
+                {{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                Reset
+            </div></center>
+        </div>
+    );
 }
